8723bfdd35d4b842900d806401fe1c6aa5e0acdb
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-11 11:48:38 +0530

    Add project files.

---
+++ OOPS-Begnr/Parameterized.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Begnr
+{
+    internal class Parameterized
+    {
+        class myclass
+        {
+            public void add (int a , int b)
+            {
+                int c = a + b;
+                Console.WriteLine(" addition of two number is  {0}", c); 
+            }
+            public void greet(int x)
+            {
+                Console.WriteLine("hello welcome to methods  {0}", x);
+            }
+         }
+        static void Main(string[] args)
+        {
+            myclass obj= new myclass();
+            obj.add(55, 90);
+            obj.greet(30);
+        }
+    }
+}
---
+++ OOPS-Begnr/Parameterless.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Begnr
+{
+    internal class Parameterless
+    {
+        class myclass
+        {
+            public void greet()
+            {
+                Console.WriteLine("Hello welcome to methods");
+            }
+
+            public void add()
+            {
+                int a = 10;
+                int b = 30;
+                int c = a + b;
+                Console.WriteLine("Addition of two numbers is : {0}", c);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            myclass obj = new myclass();
+            obj.greet();
+            obj.add();
+        }
+    }
+}
