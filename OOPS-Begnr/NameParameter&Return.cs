c43c6a531d0aaf19ba1ea58a4990b4f0c406e180
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-11 12:50:25 +0530

    new line

---
+++ OOPS-Begnr/NameParameter&Return.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Begnr
+{
+    internal class NameParameter_Return
+    {
+        class myclass
+        {
+            public string display(string name)
+            {
+                
+                string c;
+                c = name;
+                return c;
+            }
+        }
+        static void Main(string[] args)
+        {
+            myclass obj = new myclass();
+            string result = obj.display("nitish");
+            Console.WriteLine("Your name is {0}", result);
+        }
+    }
+}
--- OOPS-Begnr/NameReturn.cs
+++ OOPS-Begnr/NameReturn.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS_Begnr
-{
-    internal class NameReturn
-    {
-        class myclass
-        {
-            public string display()
-            {
-                string name="nitish";
-                string c;
-                c = name;
-                return c;
-            }
-           
-        }
-        static void Main(string[] args)
-        {
-            myclass obj= new myclass();
-            string result= obj.display();
-            Console.WriteLine("your name is {0}", result);
-        }
-    }
-}
+//namespace OOPS_Begnr
+//{
+//    internal class NameReturn
+//    {
+//        class myclass
+//        {
+//            public string display()
+//            {
+//                string name = "nitish";
+//                string c;
+//                c = name;
+//                return c;
+//            }
+
+//        }
+//        static void Main(string[] args)
+//        {
+//            myclass obj = new myclass();
+//            string result = obj.display();
+//            Console.WriteLine("your name is {0}", result);
+//        }
+//    }
+//}
