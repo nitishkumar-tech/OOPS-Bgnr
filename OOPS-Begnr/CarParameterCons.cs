702d99c3948b4263ad06081e8bbe2bcce3ff490b
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-15 12:31:42 +0530

    new line inserted

--- OOPS-Begnr/CarParameterCons.cs
+++ OOPS-Begnr/CarParameterCons.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,22 +16,36 @@ namespace OOPS_Begnr
             int year;
 
 
-            public car(string model, string color, int year)
+            public car(string Model, string Color, int Year)
             {
-                this.model = model;
-                this.color = color;
-                this.year = year;
+                model = Model;
+                color = Color;
+                year = Year;
+                if (year <= 1885)
+                {
+                    Console.WriteLine("car not found");
+                }
+                else
+                {
+                    Console.WriteLine("car manufactured year is :" + year);
+                }
             }
             public void cardisplay()
             {
                 Console.WriteLine("car mode is :" + model);
                 Console.WriteLine("car color is :" + color);
-                Console.WriteLine("car manufactured year is :" + year);
+
+
             }
         }
         static void Main(string[] args)
         {
-            car obj = new car("BMW", "Blue", 2025);
+            
+
+            car obj = new car("BMW", "Blue", 1886);
+                obj.cardisplay();
+
+            car obj1 = new car("Audi", "red", 1885);
             obj.cardisplay();
         }
     }
