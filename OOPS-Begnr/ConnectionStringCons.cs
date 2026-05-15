0ae48f6af7dd63eb5844b1fab82a2bb1d61e04ef
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-15 11:32:16 +0530

    push

---
+++ OOPS-Begnr/ConnectionStringCons.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Begnr
+{
+    internal class ConnectionStringCons
+    {
+        class database
+        {
+            string connectionstring;
+            SqlConnection connection;
+
+            public database(string connectionstring)
+            {
+                this.connectionstring = connectionstring;
+                connection = new SqlConnection(connectionstring);
+            }
+
+            public void openconnection()
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("database connection is opened");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            public void closeconnection()
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                    Console.WriteLine("database connection is closed");
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                string connstring =
+                "server=.\\SQLEXPRESS;Initial Catalog=ado;Integrated Security=true";
+                database db = new database(connstring);
+                db.openconnection();
+                db.closeconnection();
+            }
+        }
+    }
+}
