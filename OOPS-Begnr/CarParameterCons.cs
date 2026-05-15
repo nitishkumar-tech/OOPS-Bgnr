747a00b18df17c3c038adcc41aef5c2f86a7717c
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-15 12:04:21 +0530

    new line is pushed

---
+++ OOPS-Begnr/CarParameterCons.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Begnr
+{
+    internal class CarParameterCons
+    {
+        class car
+        {
+            string model;
+            string color;
+            int year;
+
+
+            public car(string model, string color, int year)
+            {
+                this.model = model;
+                this.color = color;
+                this.year = year;
+            }
+            public void cardisplay()
+            {
+                Console.WriteLine("car mode is :" + model);
+                Console.WriteLine("car color is :" + color);
+                Console.WriteLine("car manufactured year is :" + year);
+            }
+        }
+        static void Main(string[] args)
+        {
+            car obj = new car("BMW", "Blue", 2025);
+            obj.cardisplay();
+        }
+    }
+}
