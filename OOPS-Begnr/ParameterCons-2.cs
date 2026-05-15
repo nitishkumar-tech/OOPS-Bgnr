271fbd37f2810a2fbaf52a9b5c2ebb0817f6ce22
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-15 10:36:37 +0530

    new line is pushed

--- OOPS-Begnr/ParameterCons-2.cs
+++ OOPS-Begnr/ParameterCons-2.cs
@@ -1,49 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS_Begnr
-{
-    internal class ParameterCons_2
-    {
-        class Employee
-        {
-            int Eid;
-            string Ename;
-            double Esal;
+//namespace OOPS_Begnr
+//{
+//    internal class ParameterCons_2
+//    {
+//        class Employee
+//        {
+//            int Eid;
+//            string Ename;
+//            double Esal;
 
 
-            public Employee()
-            {
-                Eid = 101;
-                Ename = "Rama";
-                Esal = 30000;
-            }
+//            public Employee()
+//            {
+//                Eid = 101;
+//                Ename = "Rama";
+//                Esal = 30000;
+//            }
 
-            public Employee(int eid, string ename, double esal)
-            {
-                Eid = eid;
-                Ename = ename;
-                Esal = esal;
-            }
+//            public Employee(int eid, string ename, double esal)
+//            {
+//                Eid = eid;
+//                Ename = ename;
+//                Esal = esal;
+//            }
 
-            public void  EmployeeDisplay()
-            {
-                Console.WriteLine("employee id is " + Eid); 
-                Console.WriteLine("employee name is " + Ename);
-                Console.WriteLine("employee salary is " + Esal);
+//            public void  EmployeeDisplay()
+//            {
+//                Console.WriteLine("employee id is " + Eid); 
+//                Console.WriteLine("employee name is " + Ename);
+//                Console.WriteLine("employee salary is " + Esal);
 
-            }
-        }
-        static void Main(string[] args)
-        {
-            Employee obj= new Employee();
-            obj.EmployeeDisplay();
-            Employee obj1 = new Employee(102, "ravi", 45000);
-            obj1.EmployeeDisplay();
-        }
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            Employee obj= new Employee();
+//            obj.EmployeeDisplay();
+//            Employee obj1 = new Employee(102, "ravi", 45000);
+//            obj1.EmployeeDisplay();
+//        }
 
-    }
-}
+//    }
+//}
---
+++ OOPS-Begnr/studentParameterizedCons.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Begnr
+{
+    internal class studentParameterizedCons
+    {
+        class student
+        {
+            int Sid;
+            string Sname;
+            double Sfee;
+
+            public student()
+            {
+                Sid = 104;
+                Sname = "Shiva";
+                Sfee = 39000;
+            }
+
+            public student(int Sid, string Sname, double Sfee)
+            {
+                this.Sid = Sid;
+                this.Sname = Sname;
+                this.Sfee = Sfee;
+            }
+
+            public void studentDisplay()
+            {
+                Console.WriteLine("Student id is :" + Sid);
+                Console.WriteLine("Student Name is " + Sname);
+                Console.WriteLine("Student fee is {0}", Sfee);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            student obj = new student();
+            obj.studentDisplay();
+            student obj1 = new student(102, "ravi", 78000);
+            obj1.studentDisplay();
+        }
+    }
+}
+
