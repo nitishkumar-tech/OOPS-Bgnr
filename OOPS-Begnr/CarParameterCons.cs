1c85d112984c25539b96157067a37445bcfaa411
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-16 09:31:17 +0530

    push

--- OOPS-Begnr/CarParameterCons.cs
+++ OOPS-Begnr/CarParameterCons.cs
@@ -1,52 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.Remoting.Metadata.W3cXsd2001;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS_Begnr
-{
-    internal class CarParameterCons
-    {
-        class car
-        {
-            string model;
-            string color;
-            int year;
-
-
-            public car(string Model, string Color, int Year)
-            {
-                model = Model;
-                color = Color;
-                year = Year;
-                if (year <= 1885)
-                {
-                    Console.WriteLine("car not found");
-                }
-                else
-                {
-                    Console.WriteLine("car manufactured year is :" + year);
-                }
-            }
-            public void cardisplay()
-            {
-                Console.WriteLine("car mode is :" + model);
-                Console.WriteLine("car color is :" + color);
+//namespace OOPS_Begnr
+//{
+//    internal class CarParameterCons
+//    {
+//        class car
+//        {
+//            string model;
+//            string color;
+//            int year;
 
 
-            }
-        }
-        static void Main(string[] args)
-        {
+//            public car(string Model, string Color, int Year)
+//            {
+//                model = Model;
+//                color = Color;
+//                year = Year;
+//                if (year <= 1885)
+//                {
+//                    Console.WriteLine("car not found");
+//                }
+//                else
+//                {
+//                    Console.WriteLine("car manufactured year is :" + year);
+//                }
+//            }
+//            public void cardisplay()
+//            {
+//                Console.WriteLine("car mode is :" + model);
+//                Console.WriteLine("car color is :" + color);
+
+
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
             
 
-            car obj = new car("BMW", "Blue", 1886);
-                obj.cardisplay();
+//            car obj = new car("BMW", "Blue", 1886);
+//                obj.cardisplay();
 
-            car obj1 = new car("Audi", "red", 1885);
-            obj.cardisplay();
-        }
-    }
-}
+//            car obj1 = new car("Audi", "red", 1885);
+//            obj.cardisplay();
+//        }
+//    }
+//}
---
+++ OOPS-Begnr/stdcons.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Begnr
+{
+    internal class stdcons
+    {
+        class student
+        {
+            int stdid;
+            string sname;
+            int sage;
+
+            public student(int stdid, string sname, int sage)
+            {
+                this.stdid = stdid;
+                this.sname = sname;
+                this.sage = sage;
+                if (sage >= 18 && sage <= 60)
+                {
+                    Console.WriteLine("your age is  " + sage);
+                }
+                else
+                {
+                    Console.WriteLine("Not a Valid Age");
+                }
+            }
+            public void studentdisplay()
+            {
+                Console.WriteLine("your id is " + stdid);
+                Console.WriteLine("your name is " + sname);
+            }
+        }
+        static void Main(string[] args)
+        {
+            student obj = new student(101, "rama", 10);
+            obj.studentdisplay(); 
+            student obj1 = new student(102, "ravi", 18);
+            obj1.studentdisplay();
+            student obj2 = new student(102, "raju", 65);
+            obj2.studentdisplay();
+            student obj3 = new student(102, "rakesh", 60);
+            obj3.studentdisplay();
+        }
+    }
+}
